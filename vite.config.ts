f0b7223f1bffa1069695b3d8c8c9449deed1feb4
swarnim <swarnimbandekar9@gmail.com>
2025-04-06 02:26:29 +0530

    Modified root dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/", // important for correct asset paths on Vercel
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
